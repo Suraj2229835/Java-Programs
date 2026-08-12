a8468f8423558413a42768505247ae7deafc57a9
Suraj2229835 <rutujaachyut29@gmail.com>
2026-08-12 23:09:07 +0530

    Implement addition program using Scanner
    The program prompts the user to enter two numbers and calculates their sum.

---
+++ Exeperiment 1 /Question 4.java
@@ -0,0 +1,18 @@
+Write a program of addition using scanner :
+
+import java.util.Scanner;
+public class scanAddition {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter first no.:");
+        int sum1 = sc.nextInt();
+        System.out.println("sum is"+sum);
+        sc.close();
+    }
+}
+
+
+Enter first no.: 5
+Enter second no.: 5
+sum is : 10
