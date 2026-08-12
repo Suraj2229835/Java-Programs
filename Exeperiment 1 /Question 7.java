de3508172aab60650bf0cea04f4b32a6c3722577
Suraj2229835 <rutujaachyut29@gmail.com>
2026-08-12 23:21:01 +0530

    Add star pattern display program
    Implement a star pattern display in Java.

---
+++ Exeperiment 1 /Question 7.java
@@ -0,0 +1,19 @@
+Display * pattern :
+
+public class star_pattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+Output:
+*
+* *
+* * *
+* * * *
+* * * * *
